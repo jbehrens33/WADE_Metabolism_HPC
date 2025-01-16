ad6dca86d95598c07699bb03fd25a9f601c64e3e
Jonny Behrens <qr9@ornl.gov>
2025-01-16 11:42:12 -0500

    Create 1-2025_MLE_test.R

---
+++ 2025-01_Trial/1-2025_MLE_test.R
@@ -0,0 +1,51 @@
+# Set Up
+
+knitr::opts_chunk$set(echo = FALSE, warning = FALSE, message = FALSE)
+
+## Clear environment
+rm(list = ls())
+
+## Set project directory 
+projdir <- getwd() # Make sure you have the R Project open! 
+
+# Preparing Data for Modeling
+## Step 1
+
+scripts <- list.files('code/', 
+                      pattern = '*.R', 
+                      full.names = TRUE)
+
+# source them in 
+sapply(scripts, source, .GlobalEnv)
+
+## Step 2
+
+## this fun installs streamMetabolizer (and dependencies) and rstan (and dependencies) from github
+prep_bayes_model()
+
+# if this returns an error, running this below will suffice
+# install.packages("StanHeaders", repos = c("https://mc-stan.org/r-packages/", getOption("repos")))
+# install.packages("rstan", repos = c("https://mc-stan.org/r-packages/", getOption("repos")))
+# remotes::install_github("https://github.com/appling/unitted")
+# remotes::install_github("https://github.com/DOI-USGS/streamMetabolizer")
+
+# Load in the remaining packages
+library('tidyverse')
+library('rstan')
+library('StanHeaders')
+library('unitted')
+library('streamMetabolizer')
+library(StreamPULSE)
+
+#install.packages(c('cli', 'fansi', 'vctrs'))
+lapply(c('cli', 'fansi', 'vctrs'), library, character.only = TRUE)
+
+# Model in MLE
+## Step 4
+
+# the user will have to define which years (calendar) they are interested in
+model_mle(years = 2021)
+
+# evaluate the MLE outputs
+mle_eval()
+```
