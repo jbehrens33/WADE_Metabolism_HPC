4622ed5255563615c6fe889abeef4c6be2ce06a6
Jonny Behrens <qr9@ornl.gov>
2025-01-22 17:28:28 -0500

    Update package_download.R

--- 2025-01_Trial/package_download.R
+++ 2025-01_Trial/package_download.R
@@ -1,6 +1,6 @@
 options(repos = "https://cran.rstudio.com/")
 
 library("remotes")
-install_version("groundhog", version = "3.1.2", repos = "http://cran.us.r-project.org")
+install_version("groundhog", version = "3.1.2", repos = "https://cran.rstudio.com")
 
 groundhog.library("tidyverse", "2023-04-22")
