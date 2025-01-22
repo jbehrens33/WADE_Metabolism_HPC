6a6aca74392d63860c1eec27a525c4c06a135b54
Jonny Behrens <qr9@ornl.gov>
2025-01-22 15:45:38 -0500

    Create run_script_test.sh

---
+++ 2025-01_Trial/run_script_test.sh
@@ -0,0 +1,13 @@
+#SBATCH -A cli185
+#SBATCH -J trialRpackageload
+#SBATCH -o %x-%j.out
+#SBATCH -t 0:30:00
+#SBATCH -p batch
+#SBATCH -N 1
+#SBATCH --mem=8GB
+
+module load r
+module load gcc
+module load openblas
+
+Rscript package_download.R
