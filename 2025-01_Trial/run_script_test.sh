6b276a8ce6d304b0de14ef6376884c03d2e6cf24
Jonny Behrens <qr9@ornl.gov>
2025-01-23 11:22:29 -0500

    Update run_script_test.sh

--- 2025-01_Trial/run_script_test.sh
+++ 2025-01_Trial/run_script_test.sh
@@ -11,4 +11,10 @@ module load r
 module load gcc
 module load openblas
 
+export all_proxy=socks://proxy.ccs.ornl.gov:3128/
+export ftp_proxy=ftp://proxy.ccs.ornl.gov:3128/
+export http_proxy=http://proxy.ccs.ornl.gov:3128/
+export https_proxy=http://proxy.ccs.ornl.gov:3128/
+export no_proxy='localhost,127.0.0.0/8,*.ccs.ornl.gov' 
+
 Rscript package_download.R
