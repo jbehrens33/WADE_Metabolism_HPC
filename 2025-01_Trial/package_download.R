7150c3fd65437d22a83fba66f7364a37b13c6592
Jonny Behrens <qr9@ornl.gov>
2025-01-23 12:41:37 -0500

    Update package_download.R

--- 2025-01_Trial/package_download.R
+++ 2025-01_Trial/package_download.R
@@ -1,13 +1,29 @@
-options(repos = "https://cran.rstudio.com")
+#options(repos = "https://cran.rstudio.com")
 
-#library("remotes")
-library("groundhog")
+#install.packages("remotes")
 # Feel free to update below depending on the version of R loaded on CADES
 #install_version("groundhog", version = "3.1.2", repos = "https://cran.rstudio.com")
 
+# Libraries
+library("remotes")
+#library("groundhog")
+
+remotes::install_github('appling/unitted')
+
+install.packages("rstan", repos = c('https://stan-dev.r-universe.dev', getOption("repos")))
+
+install.packages("StanHeaders", repos = c("https://mc-stan.org/r-packages/", getOption("repos")))
+
+remotes::install_github(
+  "DOI-USGS/streamMetabolizer", # soon to be "DOI-USGS/streamMetabolizer"
+  build_vignettes = TRUE)
+
+# install.packages("rstan", repos = c("https://mc-stan.org/r-packages/", getOption("repos")))
+
+
 # This will download libraries based on the version available a month after
 # the release of the version of R that is loaded into CADES
-packages<-c("tidyverse")#, 
-            #"unitted", "StanHeaders", "rstan")
-
-groundhog.library(packages, "2023-04-22")
+# packages<-c("tidyverse")#, 
+#             #"unitted", "StanHeaders", "rstan")
+# 
+# groundhog.library(packages, "2023-04-22")
